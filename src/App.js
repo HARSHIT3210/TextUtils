f70166019075d389559c022464cc4606d19aada4
HARSHIT3210 <140914170+HARSHIT3210@users.noreply.github.com>
2023-11-27 23:07:25 +0530

    Added Changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
 import './App.css';
+import { useState } from 'react';
+// import About from './Components/About';
 import Navbar from './Components/Navbar';
 import TextForm from './Components/TextForm';
+import Alert from './Components/Alert';
+
+
 function App() {
-  return (
+  const [mode, setkMode] = useState('light'); // This will tell if darkmode is enabled or not.
+
+  const toggleMode = () => {
+    if(mode === 'light'){
+      setkMode('dark')
+      document.body.style.backgroundColor = 'gray';
+    }
+    else{
+      setkMode('light')
+      document.body.style.backgroundColor = 'white';
+    }
+  }
+    return (
     <>
-  <Navbar title = "textutils" aboutText = "About Us5"></Navbar>
-  <div className='container'>
-    <TextForm heading = "Enter The Text"></TextForm>
+  <Navbar title = "TextUtils" aboutText = "About Us" mode={mode} toggleMode={toggleMode}/>
+  <Alert alert="This is Alert"/>
+  <div className='container my-3'>
+    <TextForm heading = "Enter The Text" mode={mode}></TextForm>
+    {/* <About/> */}
+
   </div>
+  
   </>
   );
 }
 
 export default App;
+
--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 
 export default function TextForm(props) {
+  const [text, setText] = useState('');
+  const [isBold, setIsBold] = useState(false);
+
   const handleUpClick = () => {
     let newText = text.toUpperCase();
     setText(newText);
@@ -11,22 +14,33 @@ export default function TextForm(props) {
     setText(newText);
   };
 
+  const cleartext = () => {
+    let newText = ' ';
+    setText(newText);
+  };
+
+  const handleBoldClick = () => {
+    setIsBold(!isBold);
+  };
+
   const handleOnChange = (event) => {
     setText(event.target.value);
   };
 
-  const [text, setText] = useState('');
-
   return (
     <>
-      <div className="container">
+      <div className="container" style={{ color: props.mode === 'dark' ? 'dark' : 'black' }}>
         <h1>{props.heading}</h1>
-        <div className="mb-3"><textarea className="form-control" value={text} onChange={handleOnChange} id="mybox" rows="8"></textarea>
+        <div className="mb-3">
+          <textarea className="form-control"
+            value={text} onChange={handleOnChange} style={{ fontWeight: isBold ? 'bold' : 'normal', backgroundColor: props.mode === 'dark' ? 'gray' : 'white', color: props.mode === 'dark' ? 'dark' : 'black' }} id="myBox" rows="8"></textarea>
         </div>
         <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert To UpperCase</button>
         <button className="btn btn-primary mx-1" onClick={handleLowClick}>Convert To LowerCase</button>
+        <button className="btn btn-danger mx-1" onClick={cleartext}>Clear Text</button>
+        <button className="btn btn-warning mx-1" onClick={handleBoldClick}>{isBold ? 'Make Normal' : 'Make Bold'}</button>
       </div>
-      <div className="container my-2">
+      <div className="container my-2" style={{ color: props.mode === 'dark' ? 'dark' : 'black' }}>
         <h2>Your Text Summary</h2>
         <p>{text.split(' ').length} Words and {text.length} characters</p>
         <p>This text is read in {0.008 * text.split(' ').length} minutes.</p>
@@ -36,3 +50,82 @@ export default function TextForm(props) {
     </>
   );
 }
+
+
+// import React, { useState } from 'react';
+
+// export default function TextForm(props) {
+//   const [text, setText] = useState('');
+//   const [textTransform, setTextTransform] = useState('normal');
+
+//   const handleTransformClick = (transformType) => {
+//     let newText = '';
+
+//     switch (transformType) {
+//       case 'uppercase':
+//         newText = text.toUpperCase();
+//         setTextTransform('uppercase');
+//         break;
+//       case 'lowercase':
+//         newText = text.toLowerCase();
+//         setTextTransform('lowercase');
+//         break;
+//       case 'normal':
+//         newText = text;
+//         setTextTransform('normal');
+//         break;
+//       default:
+//         newText = text;
+//         setTextTransform('normal');
+//         break;
+//     }
+
+//     setText(newText);
+//   };
+
+//   const cleartext = () => {
+//     let newText = ' ';
+//     setText(newText);
+//     setTextTransform('normal');
+//   };
+
+//   const handleOnChange = (event) => {
+//     setText(event.target.value);
+//   };
+
+//   return (
+//     <>
+//       <div className="container">
+//         <h1>{props.heading}</h1>
+//         <div className="mb-3">
+//           <textarea
+//             className="form-control"
+//             value={text}
+//             onChange={handleOnChange}
+//             style={{ textTransform: textTransform }}
+//             rows="8"
+//           ></textarea>
+//         </div>
+//         <button className="btn btn-primary mx-1" onClick={() => handleTransformClick('uppercase')}>
+//           Convert To UpperCase
+//         </button>
+//         <button className="btn btn-primary mx-1" onClick={() => handleTransformClick('lowercase')}>
+//           Convert To LowerCase
+//         </button>
+//         <button className="btn btn-danger mx-1" onClick={cleartext}>
+//           Clear Text
+//         </button>
+//         <button className="btn btn-warning mx-1" onClick={() => handleTransformClick('normal')}>
+//           Make Normal
+//         </button>
+//       </div>
+//       <div className="container my-2">
+//         <h2>Your Text Summary</h2>
+//         <p>{text.split(' ').length} Words and {text.length} characters</p>
+//         <p>This text is read in {0.008 * text.split(' ').length} minutes.</p>
+//         <h2>Preview</h2>
+//         <p>{text}</p>
+//       </div>
+//     </>
+//   );
+// }
